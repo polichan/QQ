7133156ecedd3fa2e477f212b1245b7a9bd9a5e6
Hexwell <pocoa@foxmail.com>
2021-11-13 19:48:25 +0800

    update(models): 去除 QQ 小程序中不存在的结构体成员

--- miniprogram/encryptor/encryptor.go
+++ miniprogram/encryptor/encryptor.go
@@ -36,21 +36,16 @@ var (
 
 // PlainData 用户信息/手机号信息
 type PlainData struct {
-	OpenID          string `json:"openId"`
-	UnionID         string `json:"unionId"`
-	NickName        string `json:"nickName"`
-	Gender          int    `json:"gender"`
-	City            string `json:"city"`
-	Province        string `json:"province"`
-	Country         string `json:"country"`
-	AvatarURL       string `json:"avatarUrl"`
-	Language        string `json:"language"`
-	PhoneNumber     string `json:"phoneNumber"`
-	OpenGID         string `json:"openGId"`
-	MsgTicket       string `json:"msgTicket"`
-	PurePhoneNumber string `json:"purePhoneNumber"`
-	CountryCode     string `json:"countryCode"`
-	Watermark       struct {
+	OpenID      string `json:"openId"`
+	UnionID     string `json:"unionId"`
+	NickName    string `json:"nickName"`
+	Gender      int    `json:"gender"`
+	City        string `json:"city"`
+	Province    string `json:"province"`
+	Country     string `json:"country"`
+	AvatarURL   string `json:"avatarUrl"`
+	PhoneNumber string `json:"phoneNumber"`
+	Watermark   struct {
 		Timestamp int64  `json:"timestamp"`
 		AppID     string `json:"appid"`
 	} `json:"watermark"`
